aa1063f58d8b0692cb416823fa2f33911dfe61c2
vsteve96 <>
2023-12-10 02:45:53 +0000

    Retrieve latest stock numbers from spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -21,7 +22,7 @@ def get_sales_data():
     """
     while True:
         print("Please enter sales data from the last market.")
-        print("Data should be six numbers, seperated by commas.")
+        print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
@@ -32,7 +33,7 @@ def get_sales_data():
             print("Data is valid!")
             break
 
-    return sales_data        
+    return sales_data
 
 def validate_data(values):
     """
@@ -55,14 +56,33 @@ def validate_data(values):
 
 def update_sales_worksheet(data):
     """
-    Update sales worksheet, add new row with the list data provided.
+    Update sales worksheet, add new row with the list data provided
     """
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+
+print("Welcome to Love Sandwiches Data Automation")
+main()
