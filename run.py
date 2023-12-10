3ed764e0304f55a6a11231dfed3e0889d9e67383
vsteve96 <>
2023-12-10 22:01:03 +0000

    Refactor update worksheet functions into one

--- run.py
+++ run.py
@@ -53,6 +53,9 @@ def validate_data(values):
         return False
 
     return True
+"""
+This part of the code is not used anymore
+Has been refactored into the update_worksheet function
 
 def update_sales_worksheet(data):
     """
@@ -70,7 +73,19 @@ def update_surplus_worksheet(data):
     print("Updating surplus worksheet...\n")
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
+    print("Surplus worksheet updated successfully.\n")
+"""
+
+def update_worksheet(data, worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
+
 
 def calculate_surplus_data(sales_row):
     """
@@ -97,9 +112,10 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
+
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
